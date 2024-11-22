550a140ecf530cd0bc792cdb93caedec75e5bcd9
sanjay <cs@DESKTOP-Q2Q54VN>
2024-11-22 13:10:52 +0530

    Adding New component for listing contact data as index or home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactHomeComponent } from './Component/contact-home/contact-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: ContactHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
